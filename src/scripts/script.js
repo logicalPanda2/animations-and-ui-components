1d784b97d80710429379de938b76510b000d54b8
Marcel <marcelrg.business@gmail.com>
2025-07-09 15:15:49 -0800

    Added smooth scrolling

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,56 @@
-// placeholder: all additional timings or logic will be handled here
+const title1 = document.getElementById("ui-ux-components-title");
+const link1 = document.getElementById("ui-ux-components-link");
+const title2 = document.getElementById("general-animations-title");
+const link2 = document.getElementById("general-animations-link");
+const title3 = document.getElementById("microinteractions-title");
+const link3 = document.getElementById("microinteractions-link");
+const titleMarginConstant = 60;
+let isScrolling = false;
+
+link1.onclick = () => {
+    if(!isScrolling) {
+        scrollToTitle(title1);
+    }
+}
+
+link2.onclick = () => {
+    if(!isScrolling) {
+        scrollToTitle(title2);
+    }
+}
+
+link3.onclick = () => {
+    if(!isScrolling) {
+        scrollToTitle(title3);
+    }
+}
+
+function scrollToTitle(titleElement, currentPosition = window.pageYOffset) {
+    isScrolling = true;
+    let newPosition;
+    let elementPosition = titleElement.getBoundingClientRect().top - titleMarginConstant;
+    let targetPosition = currentPosition + elementPosition;
+    if(currentPosition < targetPosition) {
+        newPosition = currentPosition + 8;
+        window.scrollTo(0, newPosition);
+        if(newPosition >= targetPosition) {
+            isScrolling = false;
+            return 0;
+        } else {
+            setTimeout(() => {
+                scrollToTitle(titleElement, newPosition);
+            }, 4);
+        }
+    } else {
+        newPosition = currentPosition - 8;
+        window.scrollTo(0, newPosition);
+        if(newPosition <= targetPosition) {
+            isScrolling = false;
+            return 0;
+        } else {
+            setTimeout(() => {
+                scrollToTitle(titleElement, newPosition);
+            }, 4);
+        }
+    }
+}
